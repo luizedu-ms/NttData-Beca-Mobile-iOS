621a56876f6865f762b93dde277bbfe3394780e7
Luiz Eduardo <luedu159@gmail.com>
2022-02-14 12:46:14 -0300

    Segundo Commit - Trabalhando no layout principal

--- AvaliacaoBecaIOS/ViewController.swift
+++ AvaliacaoBecaIOS/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var destaquesCollectionView: UICollectionView!
+
+    var posterArray = [UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "2"),]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        destaquesCollectionView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return posterArray.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell: UICollectionViewCell = destaquesCollectionView.dequeueReusableCell(withReuseIdentifier: "destaqueCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
